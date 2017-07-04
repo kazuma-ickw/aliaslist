c6127645ba4340189a68cdb77a04bd378d6d9e7f
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-05 00:46:05 +0900

    create show command showing all alias list

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/urfave/cli"
 )
@@ -10,6 +12,21 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "aliaslist"
 	app.Usage = "show unused aliases"
+
+	app.Action = func(c *cli.Context) error {
+		if c.Args().Get(0) == "show" {
+			out, err := exec.Command("sh", "-c", "source ~/.bashrc && alias").Output()
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println(string(out))
+			return nil
+		}
+
+		return nil
+	}
 
 	app.Run(os.Args)
 }
